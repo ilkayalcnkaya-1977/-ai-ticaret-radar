9beaa35d5fbee2ced2971d7c78061e263cab0304
ilkayalcnkaya-1977 <ilkayalcnkaya@gmail.com>
2026-09-02 23:39:37 +0300

    Update radar.js

--- api/radar.js
+++ api/radar.js
@@ -91,7 +91,7 @@ export default async function handler(req, res) {
     }
 
     if (
-      /iphone|ıphone|telefon|samsung|xiaomi|tablet|bilgisayar|laptop|kulaklık|airpods|dyson|teknoloji|kamera/.test(
+      
         text
       )
     ) {
